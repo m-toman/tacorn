a416844817aceeb2e93f017df4614dc1742dff64
m-toman <m.toman@neuratec.com>
2018-12-06 10:26:21 +0100

    Tacotron2 wrapper

--- tacorn/tacotron2_wrapper.py
+++ tacorn/tacotron2_wrapper.py
@@ -1,7 +1,8 @@
 """ Wraps Tacotron-2 functionality. """
-
-from collections import namedtuple
+import sys
 import multiprocessing
+from collections import namedtuple
+
 import tacorn.fileutils as fu
 from tacorn.experiment import Experiment
 
@@ -19,16 +20,18 @@ def preprocess(experiment: Experiment, args) -> None:
     """ Preprocesses wavs and text, returns None or raises an Exception. """
     # bring data in format usable by Tacotron-2
     # for now just copy them over
-    raw_wav_dir = os.path.join(experiment.paths["feature_model"], "LJSpeech-1.1", "wavs")
+    raw_wav_dir = os.path.join(
+        experiment.paths["feature_model"], "LJSpeech-1.1", "wavs")
     fu.ensure_dir(raw_wav_dir)
     fu.copy_files(args["wav_dir"], raw_wav_dir)
-    raw_metadata_file = os.path.join(experiment.paths["feature_model"], "LJSpeech-1.1", "metadata.csv")
+    raw_metadata_file = os.path.join(
+        experiment.paths["feature_model"], "LJSpeech-1.1", "metadata.csv")
     fu.copy_file(args["text_file"], raw_metadata_file)
 
     # run Tacotron-2 preprocessing
     args = namedtuple(
         "tacoargs", "base_dir hparams dataset language voice reader merge_books book output n_jobs".split())
-    args.base_dir = experiment.paths["root"]
+    args.base_dir = experiment.paths["feature_model"]
     args.language = experiment.config["language"]
     args.output = experiment.paths["features"]
     args.hparams = ""
@@ -44,6 +47,17 @@ def preprocess(experiment: Experiment, args) -> None:
     tacotron2.preprocess.run_preprocess(args, modified_hp)
 
 
-def create(experiment: Experiment, args) -> None:
-    """ Creates Tacotron-2 specific folders and configuration. """
-    return
+def train(experiment: Experiment, args) -> None:
+    """ Trains a Tacotron-2 model. """
+    args = namedtuple(
+        "tacoargs", "base_dir hparams tacotron_input name model input_dir".split())  # name?
+    args.base_dir = experiment.paths["feature_model"]
+    args.hparams = ''
+    # args.name
+    args.tacotron_input = os.path.join(
+        experiment.paths["feature_model"], "training_data", "train.txt")
+    args.input_dir = 'training_data'
+    args.model = 'Tacotron'
+
+    log_dir, hparams = tacotron2.train.prepare_run(args)
+    tacotron2.tacotron.train.tacotron_train(args, log_dir, hparams)
